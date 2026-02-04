6dc0059875d21b675da95efc2c7dad5131c7cf4d
Ohkubo KOHEI <o@kbn.one>
2026-02-04 03:32:55 +0000

    add bundle task

--- server/app.ts
+++ server/app.ts
@@ -3,9 +3,8 @@ import { createBundleServeMiddleware } from "./bundleServe.ts";
 
 import { Hono } from "@hono/hono";
 import { cors } from "@hono/hono/cors";
+import { serveStatic } from "@hono/hono/deno";
 import { driveFiles } from "./gdrive.ts";
-
-const bundleResult = await bundle(false);
 
 const app: Hono = new Hono();
 app
@@ -22,8 +21,28 @@ app
     return c.json(response);
   });
 
+async function clientServer() {
+  try {
+    const distPath = new URL("../dist", import.meta.url).pathname;
+    const st = await Deno.stat(distPath);
+    if (st.isDirectory) {
+      return serveStatic({ root: distPath, rewriteRequestPath(path) {
+        if (path === "/") return "/index.html";
+        return path;
+      }, });
+    }
+  } catch (e) {
+    if (!(e instanceof Deno.errors.NotFound)) {
+      console.error(e);
+    }
+  }
+
+  console.log("Bundling client...");
+  const bundleResult = await bundle(false);
+  return createBundleServeMiddleware(bundleResult);
+}
 // Serve bundled application using Deno.bundle
-app.get("/*", createBundleServeMiddleware(bundleResult))
+app.get("/*", await clientServer())
   .get("*", (c) => c.text("Not Found", 404))
   .onError((err, c) => {
     console.error(err);
